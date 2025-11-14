08b388c0193ddee5967a855fc6d59b4c26e4a879
Hernan <perezhernan59@gmail.com>
2025-11-13 22:22:20 -0300

    Initial config

---
+++ futbol-api-challenge/src/test/java/com/ideapp/futboapi/FutbolApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideapp.futboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
